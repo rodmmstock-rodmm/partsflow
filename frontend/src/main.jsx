8eb6852e04845e6939b8cf5d3f7711bb5925a73f
rodmmstock-rodmm <rodmmstock@gmail.com>
2026-09-11 13:06:16 +0700

    ui: load Order V9 refinement styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,6 +10,7 @@ import "./styles.css";
 import "./searchable-select.css";
 import "./order-focus-v8.css";
 import "./order-v9.css";
+import "./order-v9-refine.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
