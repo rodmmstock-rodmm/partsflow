f6b2cb9fbb8f4782d2f13dea86814b3693b1aa6e
rodmmstock-rodmm <rodmmstock@gmail.com>
2026-09-11 15:00:17 +0700

    ui: enable approved production navigation and Order UI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./auth";
 import { OptionsProvider } from "./optionsContext";
+import ProductionApprovedUi from "./ProductionApprovedUi";
 import { installOrderSelectedExcelExport } from "./orderSelectedExcelExport";
 import { installOrderDetailEnhancer } from "./orderDetailEnhancer";
 import "./styles.css";
@@ -11,10 +12,12 @@ import "./searchable-select.css";
 import "./order-focus-v8.css";
 import "./order-v9.css";
 import "./order-v9-refine.css";
+import "./production-approved-theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ProductionApprovedUi />
       <AuthProvider>
         <OptionsProvider>
           <App />
